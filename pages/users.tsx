e6723663a0a66bd24bd361ed46963fe80621865b
Daisuke <d.suke.kida@gmail.com>
2023-09-16 09:01:47 +0900

    ts導入

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,23 +1,23 @@
 import { useState, useEffect } from "react";
 
 export default function Users() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
   useEffect(() => {
     const fetchUser = async () => {
-      const res = await fetch('./api/users')
-      const data = await res.json()
-      setUsers(data.users)
-    }
-    fetchUser()
-  }, [])
+      const res = await fetch("./api/users");
+      const data = await res.json();
+      setUsers(data.users);
+    };
+    fetchUser();
+  }, []);
 
   return (
     <div>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>{ user.name }</li>
+          <li key={user.id}>{user.name}</li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
